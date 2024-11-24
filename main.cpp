2870ce4f2b2a08ee502f64e17efd6b154ec6901b
Makar <makar.mologkov@gmail.com>
2024-11-24 19:36:39 +0500

    Реализация 1-ой функции, создание текстового файла для чтения

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
-//erererwr
 void f_1(){
-
+ifstream readfile("read.txt");
+vector <string> readvector;
+string line;
+while(getline(readfile,line)){
+        readvector.push_back(line);
+        //cout<<line<<endl;
+}
+readfile.close();
 }
 
 void f_2(){
