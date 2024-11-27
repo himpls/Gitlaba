2b39991066149858a336b64d5270b19afa64ca74
Maks Generalov <generalov.maks.84@yandex.ru>
2024-11-27 22:02:20 +0500

    Реализация второй и третьей функции

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,16 +9,45 @@ void f_1(){
 
 } //commit after synchronization
 
-void f_2(){
+void f_2(vector<string> &readvector){
+/*string path="read.txt";
+ifstream in;
+in.open(path);
 
+if(in.is_open())
+{
+    string str;
+    while(!in.eof())
+    {
+        str="";
+        getline(in,str);
+        cout<<str<<endl;
+    }
 }
+else{
+    cout<<"File not opened";
+}*/
 
+for(size_t i = 0; i < readvector.size(); ++i){
+    cout << readvector[i] << endl;
+      }
+  }
 void f_3(){
-
+ofstream out;
+out.open("write.txt");
+  if (out.is_open())
+  {
+      for(size_t i = 0; i < readvector.size(); i++){
+                out << readvector[i] << endl;
+      }
+  }
+  out.close();
 }
 
-int main(){
+int main()
+{
+    vector<string> readvector;
 f_1();
-f_2();
+f_2(readvector);
 f_3();
 }
